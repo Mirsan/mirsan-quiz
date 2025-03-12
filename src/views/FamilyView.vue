245c96deec31fc22ddd6a43cf44c9da68fddd8b6
Mirsan <39568605+Mirsan@users.noreply.github.com>
2025-03-12 23:29:46 +0100

    Pierwszy przycisk - wyświetlacz

--- src/views/FamilyView.vue
+++ src/views/FamilyView.vue
@@ -1,18 +1,9 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col cols="8">
-                <v-file-input label="Wybierz plik .txt" prepend-icon="mdi-file" variant="filled" accept=".txt" @change="onFileSelect"></v-file-input>
-            </v-col>    
-            <v-col  cols="4">
-                <v-btn prepend-icon="mdi-checkbox-marked" color="success" block @click="generateSongs">Generuj</v-btn>
-            </v-col>
-        </v-row>
-        <v-row v-for="song in songs" :key="song.category">
-            <v-col>
-                <v-btn prepend-icon="mdi-music" color="primary" block> {{ song.category }} </v-btn>
-            </v-col>
-        </v-row>
+        <v-chip density="compact" class="text-h1 px-16 py-16" style="background-color: black; color: yellow">
+            265
+        </v-chip>
+
     </v-container>
 </template>
 
