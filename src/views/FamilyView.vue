82692f289acf627112ea89feb041fce38e9fa68e
Mirsan <39568605+Mirsan@users.noreply.github.com>
2025-03-13 00:23:56 +0100

    Zapis przed zmianami

--- src/views/FamilyView.vue
+++ src/views/FamilyView.vue
@@ -1,6 +1,19 @@
 <template>
     <v-container>
         <SmallDigitalScreen :value="32" />
+
+        <br>
+
+        <v-card class="pa-4" outlined style="background-color: black; height: 100%; width: 100%;">
+            <v-card-text style="color: yellow; font-size: 2em; font-family: 'PixelFont';">
+                1 &nbsp; SLASK WROCLAW 35 <br>
+                2 &nbsp; LEGIA WARSZAWA 17 <br>
+                3 &nbsp; ARKA GDYNIA KURWA S 12 <br>
+                4 &nbsp; ........................................... <br>
+                5 &nbsp; ........................................... <br>
+            </v-card-text>
+        </v-card>
+
     </v-container>
 </template>
 
