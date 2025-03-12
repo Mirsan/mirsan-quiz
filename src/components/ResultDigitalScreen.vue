b67f7722ed9ed0e676d703f621bcd41217cefdde
Mirsan <39568605+Mirsan@users.noreply.github.com>
2025-03-13 00:52:04 +0100

    Dodana tablica wynikow

---
+++ src/components/ResultDigitalScreen.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="pa-4" outlined style="background-color: black; height: 100%; width: 100%;">
+    <v-card-text style="color: yellow; font-size: 2em; font-family: 'PixelFont';">
+      <div v-for="item in results" :key="item.id">
+        {{ item.id }} &nbsp; 
+        <span v-if="item.pass">
+          {{ item.name }} {{ item.points }}
+        </span>
+        <span v-if="!item.pass">
+          ...................................
+        </span>
+        <br>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'FamilyResultDigitalScreen',
+  props: {
+    results: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  },
+  mounted() {
+    console.log('Results:', this.results)
+  }
+}
+</script> 
--- src/views/FamilyView.vue
+++ src/views/FamilyView.vue
@@ -3,15 +3,8 @@
         <SmallDigitalScreen :value="32" />
 
         <br>
-
         <v-card class="pa-4" outlined style="background-color: black; height: 100%; width: 100%;">
-            <v-card-text style="color: yellow; font-size: 2em; font-family: 'PixelFont';">
-                1 &nbsp; SLASK WROCLAW 35 <br>
-                2 &nbsp; LEGIA WARSZAWA 17 <br>
-                3 &nbsp; ARKA GDYNIA KURWA S 12 <br>
-                4 &nbsp; ........................................... <br>
-                5 &nbsp; ........................................... <br>
-            </v-card-text>
+            <ResultDigitalScreen :results="results" />
         </v-card>
 
     </v-container>
@@ -20,15 +13,22 @@
 <script>
 import { defineComponent } from 'vue';
 import SmallDigitalScreen from '@/components/SmallDigitalScreen.vue';
+import ResultDigitalScreen from '@/components/ResultDigitalScreen.vue';
 
 export default defineComponent({
   name: 'FamilyView',
   components: {
-    SmallDigitalScreen
+    SmallDigitalScreen,
+    ResultDigitalScreen
   },
   data() {
     return {
-      songs: []
+      results: [
+        { id: 1, name: 'SLASK WROCLAW', points: 35, pass: false },
+        { id: 2, name: 'LEGIA WARSZAWA fdsadfsfds', points: 17, pass: true },
+        { id: 3, name: 'ARKA GDYNIA', points: 12, pass: false },
+        { id: 4, name: 'JAGIELONIA', points: 4, pass: true }
+      ]
     }
   },
   methods: {
